af0b817aaeff4a1fdc2a8393a710a9d19db8564f
RAÚL SUERO HURTADO <49399328C@DOMDPTOINFJMPP.INF>
2025-09-29 18:17:41 +0100

    Avances z12

--- Otros/z12_SalarioNeto/z12_SalarioNeto.java
+++ Otros/z12_SalarioNeto/z12_SalarioNeto.java
@@ -19,7 +19,7 @@ public class z12_SalarioNeto {
         Impuestos*/
 
         //Variables
-        Double precioPorHora = 0.0, numeroDeHoras = 0.0, pagoBruto=0.0, salarioNeto = 0.0, impuestos = 0.0, numeroDeHorasExtras=0.0, pagoPorHorasExtras = 0.0;
+        Double precioPorHora = 0.0, numeroDeHoras = 0.0, pagoBruto=0.0, salarioNeto = 0.0,impuesto25=0.0, impuesto45=0.0, impuestos = 0.0, numeroDeHorasExtras=0.0, pagoPorHorasExtras = 0.0;
 
         //Pide al usuario el precio por hora
         Scanner sc = new Scanner(System.in);
@@ -38,22 +38,30 @@ public class z12_SalarioNeto {
 
         //Calcula el pago bruto sin importar los impuestos
         pagoBruto=(precioPorHora*numeroDeHoras)+pagoPorHorasExtras;
-
+        
         //If para saber si hay que añadir impuestos del 25%
-        if (pagoBruto>500 && pagoBruto<=900) {
-                impuestos=pagoBruto*0.25;
-                salarioNeto=pagoBruto-impuestos;
+        if (pagoBruto>500) {
+                pagoBruto-=500;
+                impuesto25=pagoBruto*0.25;
+                pagoBruto+=500;
+                salarioNeto=pagoBruto-impuesto25;
         }
 
         //If para saber si hay que añadir impuestos del 45%
          if (pagoBruto>900) {
-            impuestos=pagoBruto*0.45;
-            salarioNeto=pagoBruto-impuestos;
+            pagoBruto-=900;
+            impuesto45=pagoBruto*0.45;
+            pagoBruto+=900;
+            salarioNeto=pagoBruto-impuesto45;
         }
 
-        //Else para cuando el pago bruto es menos de 500
-        else salarioNeto=pagoBruto;
+        //If para cuando el pago bruto es menos de 500
+        if (pagoBruto<500) 
+            salarioNeto=pagoBruto;
 
+        //Calcula el total de impuestos
+        impuestos=impuesto25+impuesto45;
+        
         //Imprime los datos resultantes
         System.out.println("Resultados: ");
         System.out.println("    Pago bruto "+pagoBruto);
