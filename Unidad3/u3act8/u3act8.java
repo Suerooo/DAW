96715c0175d8af5806727c7a3213f54675d2a4e5
RAÚL SUERO HURTADO <49399328C@DOMDPTOINFJMPP.INF>
2025-10-03 16:42:08 +0100

    16:41 03/10/25

---
+++ Unidad3/u3act8/u3act8.java
@@ -0,0 +1,16 @@
+package u3act8;
+
+public class u3act8 {
+    public static void main(String[] args) {
+        /*Realiza un programa que nos pida un número n y que nos diga cuantos números primos hay entre 1 y n. Un número primo es aquel que solo es divisible por 1 y por el mismo. Veamos un ejemplo para n=8.
+        1 - primo
+        2 - primo 
+        3 - primo
+        4 - no primo
+        5 - primo
+        6 - no primo
+        7 - primo
+        8 - no primo
+        5 Números primos en total */
+    }
+}
