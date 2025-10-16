d503bb324d51858d01735c1105d657b0c6178d30
Suerooo <raulsuerodaw@gmail.com>
2025-10-16 22:35:34 +0100

    22:35 16/10/25

--- Unidad4/u4act3.java
+++ Unidad4/u4act3.java
@@ -2,14 +2,41 @@ package Unidad4;
 
 import java.util.Scanner;
 
-public class u4act3 {
-    public static void main(String[] args) {
-        /*Escribir una función a la que se le pase un número entero y 
-        devuelva el número de divisores primos que tiene. */
 
+public class u4act3 {
+
+    //Método que determina si un numero es primo o no primo
+    static boolean esPrimo(int numero) {
+        boolean primo=true;
+        for (int i=2; i<numero; i++) {
+            if (numero%i==0) {
+                primo=false;
+            }
+        }
+
+        return primo;
+    }
+
+    //Método que calcula los números primos de los divisores de un numero
+    static void divisoresPrimos() {
         Scanner sc = new Scanner(System.in);
         System.out.print("Introduce un número: ");
         int numero = sc.nextInt();
 
+        for (int i=1; i<=numero; i++) {
+            if (esPrimo(i)==true) {
+                System.out.println(i+" es primo");
+            } else {
+                System.out.println(i+" no es primo");
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        /*Escribir una función a la que se le pase un número entero y 
+        devuelva el número de divisores primos que tiene. */
+
+        //Llama al método divisoresPrimos
+        divisoresPrimos();
     }
 }
