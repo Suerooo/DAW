e892257725b280d4f66c4f91b919dccc8c4407c3
Suerooo <raulsuerodaw@gmail.com>
2025-11-21 11:40:27 +0000

    Act1Inventario(TipoExamen2)

--- Java/Practica/Examen2/TipoExamen/Act1InventarioZapatillas.java
+++ Java/Practica/Examen2/TipoExamen/Act1InventarioZapatillas.java
@@ -28,25 +28,34 @@ public class Act1InventarioZapatillas {
         }
     }
 
-    static int[] agregarCodigo(int[] codigo) {
+    static int guardarCodigoNoRepetido(int[] codigo) {
         boolean estaRepetido;
-        int codigoAAgregar;
+        int codigoNoRepetido;
 
         do {
-            System.out.print("Cual es el código del producto nuevo: ");
-            codigoAAgregar=sc.nextInt();
+            System.out.print("Introduce el código del producto (escribe -1 si quieres salir): ");
+            codigoNoRepetido=sc.nextInt();
+
+            if (codigoNoRepetido==-1) {
+                return -1;
+            }
+
             estaRepetido=false;
 
             for (int i=0; i<codigo.length; i++) {
-                if (codigoAAgregar==codigo[i]) {
+                if (codigoNoRepetido==codigo[i]) {
                     System.out.println("El código esta repetido prueba con otro");
                     estaRepetido=true;
                 }
             }
         } while (estaRepetido);
+        
+        return codigoNoRepetido;
+    }
 
+    static int[] agregarCodigo(int[] codigo, int codigoNoRepetido) {
         codigo=Arrays.copyOf(codigo, codigo.length+1);
-        codigo[codigo.length-1]=codigoAAgregar;
+        codigo[codigo.length-1]=codigoNoRepetido;
         System.out.println("Código guardado\n");
 
         return codigo;
@@ -70,51 +79,81 @@ public class Act1InventarioZapatillas {
         return marca;
     }
 
-    static int 
-
-    static int[] eliminarCodigo(int[] codigo) {
+    static int encontrarIndiceProducto(int[] codigo) {
         boolean estaEnInventario;
-        int codigoAEliminar;
-        int indicecodigoAEliminar=-1;
+        int codigoABuscar;
+        int indiceProducto=-1;
 
         do {
-            System.out.print("Cual es el código del producto que quiere eliminar: ");
-            codigoAEliminar=sc.nextInt();
-            estaEnInventario=true;
+            System.out.print("Introduce el código del producto (escribe -1 si quieres salir): ");
+            codigoABuscar=sc.nextInt();
+
+            if (codigoABuscar==-1) {
+                return -1;
+            }
+
+            estaEnInventario=false;
 
             for (int i=0; i<codigo.length; i++) {
-                if (codigoAEliminar==codigo[i]) {
-                    indicecodigoAEliminar=i;
-                } else {
-                    System.out.println("El código introducido no se encuentra en el inventario");
-                    estaEnInventario=false;
+                if (codigoABuscar==codigo[i]) {
+                    indiceProducto=i;
+                    estaEnInventario=true;
                 }
             }
+
+            if (!estaEnInventario) {
+                System.out.println("El código introducido no se encuentra en el inventario");    
+            }
+
         } while (!estaEnInventario);
 
+        return indiceProducto;
+    }
+
+    static int[] eliminarCodigo(int[] codigo, int indiceProductoAEliminar) {
         int[] codigoActualizado=new int[0];
 
         for(int i=0; i<codigo.length; i++) {
-            if (indicecodigoAEliminar!=i) {
+            if (indiceProductoAEliminar!=i) {
                 codigoActualizado=Arrays.copyOf(codigoActualizado, codigoActualizado.length+1);
                 codigoActualizado[codigoActualizado.length-1]=codigo[i];
             }
         }
 
-        return codigo;
+        return codigoActualizado;
     }
 
-    static double[] eliminarPrecio(double[] precio) {
-        double[] codigoActualizado=new double[0];
+    static double[] eliminarPrecio(double[] precio, int indiceProductoAEliminar) {
+        double[] precioActualizado=new double[0];
 
         for(int i=0; i<precio.length; i++) {
-            if (indicecodigoAEliminar!=i) {
-                codigoActualizado=Arrays.copyOf(codigoActualizado, codigoActualizado.length+1);
-                codigoActualizado[codigoActualizado.length-1]=precio[i];
+            if (indiceProductoAEliminar!=i) {
+                precioActualizado=Arrays.copyOf(precioActualizado, precioActualizado.length+1);
+                precioActualizado[precioActualizado.length-1]=precio[i];
             }
         }
 
-        return precio;
+        return precioActualizado;
+    }
+
+    static String[] eliminarMarca(String[] marca, int indiceProductoAEliminar) {
+        String[] marcaActualizada=new String[0];
+
+        for(int i=0; i<marca.length; i++) {
+            if (indiceProductoAEliminar!=i) {
+                marcaActualizada=Arrays.copyOf(marcaActualizada, marcaActualizada.length+1);
+                marcaActualizada[marcaActualizada.length-1]=marca[i];
+            }
+        }
+
+        return marcaActualizada;
+    }
+
+    static void mostrarXProducto(int[] codigo, double[] precio, String[] marca, int indiceProducto) {
+        System.out.print((indiceProducto+1)+"º producto:");
+        System.out.print(" código: "+codigo[indiceProducto]);
+        System.out.print(" - precio: "+precio[indiceProducto]);
+        System.out.print(" - marca: "+marca[indiceProducto]+"\n");
     }
 
     
@@ -134,14 +173,32 @@ public class Act1InventarioZapatillas {
             switch (opcion) {
                 case 1 -> mostrarInventario(codigo, precio, marca);
                 case 2 -> {
-                    codigo=agregarCodigo(codigo);
-                    precio=agregarPrecio(precio);
-                    marca=agregarMarcar(marca);
+                    int codigoNoRepetido=guardarCodigoNoRepetido(codigo);
+
+                    if (codigoNoRepetido!=-1) {
+                        codigo=agregarCodigo(codigo, codigoNoRepetido);
+                        precio=agregarPrecio(precio);
+                        marca=agregarMarcar(marca);
+                    }
+                    
+                }
+                case 3 -> {
+                    int indiceProductoAEliminar=encontrarIndiceProducto(codigo);
+
+                    if (indiceProductoAEliminar!=-1) {
+                        codigo=eliminarCodigo(codigo, indiceProductoAEliminar);
+                        precio=eliminarPrecio(precio, indiceProductoAEliminar);
+                        marca=eliminarMarca(marca, indiceProductoAEliminar);
+                        System.out.println("Producto eliminado");
+                    }
+                }
+                case 4 -> {
+                    int indiceProducto=encontrarIndiceProducto(codigo);
+                    mostrarXProducto(codigo, precio, marca, indiceProducto);
                 }
                 case 5 -> System.out.println("Saliendo...");
                 default -> System.out.println("Esa opción no existe");
             }
-            
         } while (opcion!=5);
     }
 }
