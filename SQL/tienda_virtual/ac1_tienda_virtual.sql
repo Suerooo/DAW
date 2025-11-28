60e6b68f8e668a78904b907ec4630d8bed4173c9
Raúl Suero <raulsuerodaw@gmail.com>
2025-11-28 14:49:17 +0000

    Corrige definiciones de tablas en el esquema de la base de datos para asegurar la integridad de los datos y mejorar la estructura

--- SQL/tienda_virtual/ac1_tienda_virtual.sql
+++ SQL/tienda_virtual/ac1_tienda_virtual.sql
@@ -3,39 +3,14 @@ create database tienda_virtual;
 use tienda_virtual;
 
 create table familia(
-cod_familia numeric(3,0) auto_increment,
-deno_familia varchar(50) unique not null,
-constraint fam_cod_pk primary key (cod_familia)
-);
-
-create table producto(
-cod_producto numeric(5,0) auto_increment,
-deno_producto varchar(20) not null,
-descripcion varchar(100),
-precio_base decimal(8,2) not null,
-porc_reposicion numeric(3,0),
-unidades_minimas numeric(4,0) not null,
-cod_familia numeric (3,0) not null,
-constraint pro_cod_PK primary key (cod_producto),
-constraint pro_pre_che check(precio_base > 0),
-constraint pro_por_che check(porc_reposicion > 0),
-constraint pro_uni_che check(unidades_minimas > 0),
-constraint pro_cod_FK foreign key (cod_familia) references familia(cod_familia)
-);
-
-create database tienda_virtual;
-
-use tienda_virtual;
-
-create table familia(
-cod_familia numeric(3),
+cod_familia numeric(3) not null,
 deno_familia varchar(50) not null,
 constraint fam_cod_pk primary key (cod_familia),
 constraint fam_deno_unique unique (deno_familia)
 );
 
 create table producto(
-cod_producto numeric(5),
+cod_producto numeric(5) not null,
 deno_producto varchar(20) not null,
 descripcion varchar(100),
 precio_base decimal(8,2) not null,
@@ -50,7 +25,7 @@ constraint pro_cod_FK foreign key (cod_familia) references familia(cod_familia)
 );
 
 create table tienda(
-    cod_tienda numeric(3),
+    cod_tienda numeric(3) not null,
     deno_tienda varchar(20) not null,
     telefono varchar(11),
     codigo_postal varchar(5) not null,
@@ -62,6 +37,7 @@ create table stock(
 	cod_tienda numeric(3) not null,
     cod_producto numeric(5) not null,
     unidades numeric(6) not null,
+    constraint sto_codt_codp_PK primary key (cod_tienda, cod_producto),
     constraint sto_codt_FK foreign key (cod_tienda) references tienda(cod_tienda),
     constraint sto_codp_FK foreign key (cod_producto) references producto(cod_producto),
     constraint sto_uni_che check(unidades >= 0)
