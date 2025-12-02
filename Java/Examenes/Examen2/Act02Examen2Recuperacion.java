7e1310b7f35da6c2a2a88fcb4f7fba13a0810f38
Raúl Suero <raulsuerodaw@gmail.com>
2025-12-02 19:51:52 +0000

    Refactor code structure for improved readability and maintainability

---
+++ Java/Examenes/Examen2/Act02Examen2Recuperacion.java
@@ -0,0 +1,28 @@
+package Examenes.Examen2;
+
+import javax.print.attribute.standard.Media;
+
+public class Act02Examen2Recuperacion {
+    //
+    // Auxiliares
+    //
+
+    static void mostrarMenu() {
+        MENÚ 
+        
+        1. Añadir 1p 
+        2. Mostrar 0,5p 
+        3. Ordenar 1,5p 
+        4. Usuarios entre dos edades 0,5p 
+        5. Media de valores entre edades 1p 
+        6. Horas totales aprox 1p 
+        7. Matriz 1,5p 
+        0. Salir  
+        
+        Selecciona una opción: 
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
