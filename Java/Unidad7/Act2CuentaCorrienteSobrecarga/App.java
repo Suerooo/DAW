3394358045b388f987f32fc0c1ce754caa827289
Raúl Suero <raulsuerodaw@gmail.com>
2025-12-17 17:25:50 +0000

    update App.java, App.java and Perro.java

--- Java/Unidad7/Act2CuentaCorrienteSobrecarga/App.java
+++ Java/Unidad7/Act2CuentaCorrienteSobrecarga/App.java
@@ -1,19 +1,19 @@
 package Unidad7.Act2CuentaCorrienteSobrecarga;
 
 public class App {
-    public static void main(String[] args) {
-        CuentaCorriente cuenta1 = new CuentaCorriente("Raul", "53252353A");
-        CuentaCorriente cuenta2 = new CuentaCorriente("Pedro", "6546546A", 3232.23);
-        CuentaCorriente cuenta3 = new CuentaCorriente("64364363A", 2322.23);
+	public static void main(String[] args) {
+		CuentaCorriente cuenta1 = new CuentaCorriente("Raul", "53252353A");
+		CuentaCorriente cuenta2 = new CuentaCorriente("Pedro", "6546546A", 3232.23);
+		CuentaCorriente cuenta3 = new CuentaCorriente("64364363A", 2322.23);
 
-        cuenta1.ingresarDinero(1000);
-        cuenta1.sacarDinero(500);
-        cuenta1.mostrarInformacion();
+		cuenta1.ingresarDinero(1000);
+		cuenta1.sacarDinero(500);
+		cuenta1.mostrarInformacion();
 
-        cuenta2.mostrarInformacion();
+		cuenta2.mostrarInformacion();
 
-        cuenta3.ingresarDinero(500);
-        cuenta3.sacarDinero(20000);
-        cuenta3.mostrarInformacion();
-    }
+		cuenta3.ingresarDinero(500);
+		cuenta3.sacarDinero(20000);
+		cuenta3.mostrarInformacion();
+	}
 }
--- Java/Unidad7/Act3ClasePerro/App.java
+++ Java/Unidad7/Act3ClasePerro/App.java
@@ -1,17 +1,17 @@
 package Unidad7.Act3ClasePerro;
 
 public class App {
-    public static void main(String[] args) {
-        Perro perro1 = new Perro();
-        Perro perro2 = new Perro("Paco", "Chihuahua");
-        Perro perro3 = new Perro("Pedro", "Chihuahua", 9, "Negro");
+	public static void main(String[] args) {
+		Perro perro1 = new Perro();
+		Perro perro2 = new Perro("Paco", "Chihuahua");
+		Perro perro3 = new Perro("Pedro", "Chihuahua", 9, "Negro");
 
-        perro1.mostrarInfo();
-        perro2.mostrarInfo();
-        perro3.mostrarInfo();
+		perro1.mostrarInfo();
+		perro2.mostrarInfo();
+		perro3.mostrarInfo();
 
-        perro2.comer();
+		perro2.comer();
 
-        perro1.ladrar();
-    }
+		perro1.ladrar();
+	}
 }
