3c2f0cc8217ca21ee13fa737a614a29ce4000695
Suerooo <raulsuerodaw@gmail.com>
2025-12-19 23:39:54 +0000

    update Main.java and MarcaPagina.java update modifica_pelicula_raul.sql

--- Java/Unidad7/Act6MarcaPagina/Main.java
+++ Java/Unidad7/Act6MarcaPagina/Main.java
@@ -2,6 +2,15 @@ package Unidad7.Act6MarcaPagina;
 
 public class Main {
     public static void main(String[] args) {
+        MarcaPagina libro1 = new MarcaPagina(-2);
         
+        libro1.incrementar();
+        System.out.println(libro1.toString());
+        
+        libro1.incrementar(20);
+        System.out.println(libro1.toString());
+        
+        libro1.empezar();
+        System.out.println(libro1.toString());
     }
 }
--- Java/Unidad7/Act6MarcaPagina/MarcaPagina.java
+++ Java/Unidad7/Act6MarcaPagina/MarcaPagina.java
@@ -3,8 +3,10 @@ package Unidad7.Act6MarcaPagina;
 public class MarcaPagina {
     private int paginaActual;
 
+    public MarcaPagina() {}
+    
     public MarcaPagina(int paginaActual) {
-        this.paginaActual = paginaActual;
+        setPaginaActual(paginaActual);
     }
     
     public int getPaginaActual() {
@@ -12,7 +14,54 @@ public class MarcaPagina {
     }
 
     public void setPaginaActual(int paginaActual) {
-        this.paginaActual = paginaActual;
+        if (paginaActual < 0) {
+            this.paginaActual = 0;
+        } else {
+            this.paginaActual = paginaActual;
+        }
+    }
+    
+    public void empezar() {
+        this.paginaActual = 0;
+    }
+    
+    public void incrementar() {
+        incrementar(1);
+    }
+    
+    public void incrementar(int incremento) {
+        if (incremento <= 0) {
+            System.out.println("No puedes incrementar 0 o negativo");
+        } else {
+            this.paginaActual += incremento;
+        }
+    }
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + paginaActual;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        MarcaPagina other = (MarcaPagina) obj;
+        if (paginaActual != other.paginaActual)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Página actual: " + this.paginaActual;
     }
 
     
