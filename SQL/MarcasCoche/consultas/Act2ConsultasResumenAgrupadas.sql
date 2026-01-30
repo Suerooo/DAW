683342e03f8857cb6466c97783c3e314c927cf8c
Raúl Suero <raulsuerodaw@gmail.com>
2026-01-30 19:58:41 +0000

    Actualizar consultas SQL en Act2ConsultasResumenAgrupadas.sql para corregir conteo de ciudades y agregar agrupaciones por cifc en las ventas.

--- SQL/MarcasCoche/consultas/Act2ConsultasResumenAgrupadas.sql
+++ SQL/MarcasCoche/consultas/Act2ConsultasResumenAgrupadas.sql
@@ -1,4 +1,4 @@
-select count(ciudad) as total from marcas where ciudad="madrid";
+select count(*) as total from marcas where ciudad="madrid";
 
 select avg(cantidad) as media from distribucion;
 
@@ -6,4 +6,8 @@ select max(dni) as "dni maximo" from clientes where ciudad="madrid";
 
 select min(dni) as "minimo" from ventas where color="blanco";
 
-select cifc, avg(cantidad) as "cantidad media" from distribucion;
+select cifc, avg(cantidad) as "cantidad media" from distribucion group by cifc;
+
+select cifc, count(*) as "total" from ventas group by cifc;
+
+select cifc, count(*) as "total" from ventas where color="blanco" group by cifc;
