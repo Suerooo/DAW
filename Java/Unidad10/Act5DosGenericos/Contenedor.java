33e9e246dca77b787ae81f695a14cffbd7285902
Raúl Suero <raulsuerodaw@gmail.com>
2026-04-07 16:33:53 +0100

    feat: agregar clase Contenedor y clase Main para manejo de tipos genéricos

---
+++ Java/Unidad10/Act5DosGenericos/Contenedor.java
@@ -0,0 +1,33 @@
+package Unidad10.Act5DosGenericos;
+
+public class Contenedor<T, U> {
+    private T value1;
+    private U value2;
+
+    public Contenedor(T value1, U value2) {
+        setValue1(value1);
+        setValue2(value2);
+    }
+
+    public T getValue1() {
+        return value1;
+    }
+
+    public void setValue1(T value1) {
+        this.value1 = value1;
+    }
+
+    public U getValue2() {
+        return value2;
+    }
+
+    public void setValue2(U value2) {
+        this.value2 = value2;
+    }
+
+    @Override
+    public String toString() {
+        return value1 + " | " + value2;
+    }
+
+}
---
+++ Java/Unidad10/Act5DosGenericos/Main.java
@@ -0,0 +1,14 @@
+package Unidad10.Act5DosGenericos;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println();
+
+        Contenedor<String, Integer> contenedor1 = new Contenedor<>("Hola", 2);
+        Contenedor<Integer, String> contenedor2 = new Contenedor<>(3, "Hola");
+
+        System.out.println(contenedor1);
+        System.out.println(contenedor2);
+    }
+
+}
