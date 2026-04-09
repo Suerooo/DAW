753f8ef51d91b85bba8ebac1f0813fad8577387f
Raúl Suero <raulsuerodaw@gmail.com>
2026-04-09 19:49:50 +0100

    refactor: eliminar línea en blanco innecesaria en la clase Main

--- Java/Unidad11/Act3ColaGenerics/Lista.java
+++ Java/Unidad11/Act3ColaGenerics/Lista.java
@@ -1,23 +1,125 @@
-package Unidad11.Act2MetodoGenerico;
+package Unidad11.Act3ColaGenerics;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
-public class Main {
-    public static <U> List<U> combinar(List<U> tabla1, List<U> tabla2) {
-        List<U> resultado = new ArrayList<>();
+public class Lista<T> implements Pila<T>, Cola<T> {
+    T[] tabla;
 
-        resultado.addAll(tabla1);
-        resultado.addAll(tabla2);
+    public Lista() {
+        this.tabla = (T[]) new Object[0];
+    }
+
+    void insertarPrincipio(T nuevo) {
+        tabla = Arrays.copyOf(tabla, tabla.length + 1);
+        System.arraycopy(tabla, 0, tabla, 1, tabla.length - 1);
+        tabla[0] = nuevo;
+    }
+
+    void insertarFinal(T nuevo) {
+        tabla = Arrays.copyOf(tabla, tabla.length + 1);
+        tabla[tabla.length - 1] = nuevo;
+    }
+
+    void insertarFinal(Lista otraLista) {
+        int tamIni = tabla.length;
+        tabla = Arrays.copyOf(tabla, tabla.length + otraLista.tabla.length);
+        System.arraycopy(otraLista.tabla, 0, tabla, tamIni, otraLista.tabla.length);
+    }
+
+    void insertar(int posicion, T nuevo) {
+        tabla = Arrays.copyOf(tabla, tabla.length + 1);
+        System.arraycopy(tabla, posicion, tabla, posicion + 1,
+                tabla.length - posicion - 1);
+        tabla[posicion] = nuevo;
+    }
+
+    T eliminar(int indice) {
+        T eliminado = null;
+        if (indice >= 0 && indice < tabla.length) {
+            eliminado = tabla[indice];
+            for (int i = indice + 1; i < tabla.length; i++) {
+                tabla[i - 1] = tabla[i];
+            }
+            tabla = Arrays.copyOf(tabla, tabla.length - 1);
+        }
+        return eliminado;
+    }
+
+    T get(int indice) {
+        T resultado = null;
+        if (indice >= 0 && indice < tabla.length) {
+            resultado = tabla[indice];
+        }
         return resultado;
     }
 
-    public static void main(String[] args) {
-        List<String> tabla1 = new ArrayList<>();
-        List<String> tabla2 = new ArrayList<>();
-        tabla1.add("Hola");
-        tabla2.add("Adios");
-        
-        System.out.println(combinar(tabla1, tabla2));
+    int buscar(T claveBusqueda) {
+        int indice = -1;
+        for (int i = 0; i < tabla.length && indice == -1; i++) {
+            if (tabla[i].equals(claveBusqueda)) {
+                indice = i;
+            }
+        }
+        return indice;
     }
+
+    public int numeroElementos() {
+        return tabla.length;
+    }
+
+    public T[] getTabla() {
+        return Arrays.copyOf(tabla, tabla.length);
+    }
+
+    public static Lista concatena(Lista x, Lista y) {
+        Lista nuevaLista = new Lista();
+
+        nuevaLista.insertarFinal(x);
+        nuevaLista.insertarFinal(y);
+
+        return nuevaLista;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(tabla);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Lista<T> lista = (Lista<T>) obj;
+        T[] tabla2 = lista.getTabla();
+
+        if (tabla.length != tabla2.length) {
+            return false;
+        }
+
+        for (int i = 0; i < tabla.length; i++) {
+            if (tabla[i] != tabla2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void apilar(T numero) {
+        insertarFinal(numero);
+    }
+
+    @Override
+    public T desapilar() {
+        return eliminar(tabla.length - 1);
+    }
+
+    @Override
+    public T desencolar() {
+        return eliminar(0);
+    }
+
+    @Override
+    public void encolar(T numero) {
+        insertarFinal(numero);
+    }
+
 }
